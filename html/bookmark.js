6369b1eec9298e9fcc48e91baa87c81e8412bb51
ryanatk <ryana804@gmail.com>
2010-07-05 01:43:53 -0700

    started adding form fill to bookmark js

--- html/bookmark.js
+++ html/bookmark.js
@@ -20,6 +20,55 @@ function gaHighlight() {
   }
 }
 
+function formFill() {
+  var fake = {};
+      fake.name = randString(4) + ' ' + randString(4);
+      fake.phone = Math.round(Math.random() * 10000000000);
+      fake.address = Math.round(Math.random() * 1000) + ' ' + randString(4);
+      fake.city = randString(8);
+      fake.state = 'NV';
+      fake.zip = Math.round(Math.random() * 100000);
+
+  fieldsets = [{'id':'newShipAdr', 'fields':[
+                 {'id':'name', 'val':fake.name},
+                 {'id':'phone_number', 'val':fake.phone},
+                 {'id':'address', 'val':fake.address},
+                 {'id':'city', 'val':fake.city},
+                 {'id':'state', 'val':fake.state},
+                 {'id':'postal', 'val':fake.zip}]},
+               {'id':'newPymtType', 'fields':[
+                 {'id':'creditCard.name', 'val':fake.name},
+                 {'id':'creditCard.plainTextNumber', 'val':'4111111111111111'},
+                 {'id':'creditCard.phone_number', 'val':fake.phone},
+                 {'id':'creditCard.address', 'val':fake.address},
+                 {'id':'creditCard.city', 'val':fake.city},
+                 {'id':'creditCard.state', 'val':fake.state},
+                 {'id':'creditCard.postal', 'val':fake.zip}]}
+               ];
+
+  for (var i = 0; i < fieldsets.length; i++) {
+    var fs = fieldsets[i];
+    if (document.getElementById(fs.id)) {
+      for (var ii = 0; ii < fs.fields.length; ii++) {
+        var field = fs.fields[ii];
+        document.getElementById(field.id).value = field.val;
+      }
+    }
+  }
+
+}
+
+function randString(len) {
+  var randy = [];
+  for (var i = 0; i < len; i++) {
+    randy.push(String.fromCharCode(97 + Math.round(Math.random() * 25)));
+  }
+  return randy.join('');
+}
+
+function randSelect() {
+}
+
 function altTags() {
   var imgs = document.getElementsByTagName('IMG'),
       imgsLength = imgs.length;
@@ -46,7 +95,7 @@ function altTags() {
               '#debug-bookmark {border:2px solid #fdfdfd; -webkit-border-radius:6px; -moz-border-radius:6px;}\n',
               '#debug-bookmark {-webkit-box-shadow:1px -1px 8px #aaa; -moz-box-shadow:3px 3px 3px #999;}\n',
               '#debug-bookmark {background:#fff;}\n',
-              '#debug-bookmark a {display:block; padding:3px; font-weight:bold; text-decoration:none; color:#666;}\n',
+              '#debug-bookmark a {display:block; padding:3px; font-weight:bold; text-decoration:none; color:#669;}\n',
               '#debug-bookmark a:hover {cursor:pointer; text-decoration:underline; color:#c00;}\n',
               '#debug-bookmark a:active {color:orange;}\n');
 
@@ -56,18 +105,39 @@ function altTags() {
   var menu = document.createElement('DIV');
   menu.id = 'debug-bookmark';
 
+  var menuItems = [{'name':'gae-clicks', 'fun':gaHighlight},
+                   {'name':'form fill',  'fun':formFill},
+                   {'name':'alt tags',   'fun':altTags}
+                  ],
+      menuLength = menuItems.length;
+
+  for (var i = 0; i < menuLength; i++) {
+    var item = menuItems[i],
+        link = document.createElement('A');
+    link.innerHTML = item.name;
+    link.onclick = item.fun;
+    menu.appendChild(link);
+  }
+
+/*
   var gae = document.createElement('A');
   gae.innerHTML = 'gae-clicks';
   gae.onclick = gaHighlight;
   menu.appendChild(gae);
 
+  var fill = document.createElement('A');
+  fill.innerHTML = 'form fill';
+  fill.onclick = formFill;
+  menu.appendChild(fill);
+
   var alt = document.createElement('A');
   alt.innerHTML = 'alt tags';
   alt.onclick = altTags;
   menu.appendChild(alt);
+*/
 
   b.appendChild(menu);
-  
+
   b.onclick = function(e){
     var target = e.target;
     if (target.id && target.id == 'debug-bookmark')
