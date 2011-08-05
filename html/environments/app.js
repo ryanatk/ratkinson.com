a9b401b8d2142d8fb4d389cb26ccae9c1af28479
ratkinson <ratkinson@zappos.com>
2011-08-04 22:15:02 -0700

    setup zapMeSomeInfo in js object

--- html/environments/app.js
+++ html/environments/app.js
@@ -5,28 +5,38 @@ var http = require('http'),
 
 var sites = ['http://www.zappos.com',
              'http://vip.zappos.com',
-             'http://www.6pm.com',
-             'http://qa.zeta.zappos.com',
-             'http://qae3.zeta.zappos.com',
-            ];
+             'http://www.6pm.com'];
 
-var timestamp = new Date().getTime();
 var content = '';
+var poop;
 
 //for (var i=0; i < sites.length; i++) {
 for (var i=0; i < 1; i++) {
   var site = sites[i];
-  request({ uri:site + '/version.txt?' + timestamp }, function (error, response, body) {
+  request({ uri:site + '/zapMeSomeInfo.do' }, function (error, response, body) {
     if (error && response.statusCode !== 200) {
       console.log('Error when contacting zappos.com')
     }
 
-    content += '<h3>' + site + '</h3>';
-    content += '<p>' + body + '</p>';
+    var info = {};
+    var lines = body.split('\n'),
+        linesLen = lines.length;
+    while (linesLen--) {
+      var line = lines[linesLen];
+      if (line != '' && line.indexOf(':') >= 0) {
+        var cleanLine = line.replace(/<[^>]+>/g, '');
+        var infos = cleanLine.split(':');
+        info[infos[0]] = infos[1];
+      }
+    }
+    console.log(info);
+    poop = info['Zeta Version'];
   });
 }
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(content);
-}).listen(8124, "ratkinson.com");
+  var table = '<table><tr><td>LIVE</td><td>'+poop+'</td></tr></table>';
+  res.end(table);
+  //res.end(poop);
+}).listen(8124, "dev.ratkinson.com");
